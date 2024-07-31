8c65c00e3706b47977ba8ff71b254c413f9a5cd6
Kamil Mrowka <kamil@chicos.life>
2024-07-31 09:52:30 +0200

    Add dotenv

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = require('express')();
+require('dotenv').config();
 
 app.get('/', (req,res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port 3001');
 });
 
 
